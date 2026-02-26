bd76a3dfd6c53cd77cf209b8c7f86c1e351bbed8
Naufal Elyzar <144782193+NaufalElyzar11@users.noreply.github.com>
2026-02-27 01:06:01 +0800

    Variabel cleaning

--- electricity_demand_folder/src/01_cleaning.py
+++ electricity_demand_folder/src/01_cleaning.py
@@ -8,11 +8,11 @@ target = "electricity_demand"
 
 df = df.dropna(subset=[target])
 
-cols_to_drop = [col for col in df.columns 
+hapus_kolom = [col for col in df.columns 
                 if ("electricity" in col.lower() or "generation" in col.lower()) 
                 and col != target]
 
-df = df.drop(columns=cols_to_drop)
+df = df.drop(columns=hapus_kolom)
 
 df = df.select_dtypes(include=[np.number])
 
@@ -20,5 +20,4 @@ os.makedirs("data/hasil_proses", exist_ok=True)
 
 df.to_csv("data/hasil_proses/clean_data.csv", index=False)
 
-print("Cleaning selesai")
-print("Shape akhir:", df.shape)
+print("Shape:", df.shape)
